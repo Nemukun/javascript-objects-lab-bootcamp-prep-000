74222ddb58cbe2af9cb1296afb73ccdd3a45994b
Azumi Callanan <azucm@hotmail.com>
2017-12-05 18:35:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 
-function deleteFromObjectByKey(object, key){
-  var newObj=Object.assign({},obj);
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
   delete newObj[key];
   return newObj;
 }
